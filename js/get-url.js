b5574268905ab89389822275486a4ff082512107
Matthew Spangenberg <mspangenberg@stumbleupon.com>
2014-03-05 18:19:05 -0800

    Files Removed

--- js/get-url.js
+++ js/get-url.js
@@ -1,6 +1,6 @@
 (function(){
 
-    var pathname = window.location.pathname;
+    var pathname = parent.window.location.origin;
 
     console.log(pathname);
     return pathname;
@@ -13,3 +13,4 @@
 
 //javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://bootstrap_3.dev/js/bootstrap-3-bookmarklet.js';})();
 //javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://bootstrap_3.dev/js/bootstrap-3-bookmarklet.js';var pathname = window.location.pathname;console.log(pathname);return pathname;$(".open-new-window").attr("href", pathname);})();
+//javascript:(function(){var pathname = parent.window.location.origin;console.log(pathname);return pathname;document.body.appendChild(document.createElement('script')).src='http://bootstrap-3-grids.dev/js/bootstrap-3-bookmarklet.js';})();
